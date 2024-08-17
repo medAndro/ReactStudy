ce19d0f4068acd07f07cc2b9bd7fcf50747fca71
medAndro <medAndro@github.com>
2024-08-18 02:58:05 +0900

    [2-22] 강의 진행

--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
@@ -7,20 +8,57 @@ let Btn = styled.button`
     color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
     padding : 10px;
 `
-let NewBtn = styled.button(Btn)`
 
+let Banner = styled.div`
+    display : ${props => props.display}
 `
+
 function Detail(props){
     let {idx} = useParams();
+    let [display, setDisplay] = useState('block');
+    let [alert, setAlert] = useState(true);
+    let [numRestrict, setNumRestrict] = useState(false);
+    useEffect(()=>{
+        let a = setTimeout(()=>{
+            setDisplay(display = 'None');
+        }, 2000);
+        let b = setTimeout(()=>{
+            setAlert(alert = false);
+        }, 5000);
+        console.log("useEffect 동작 코드")
+        return()=>{
+            clearTimeout(a)
+            clearTimeout(b)
+            console.log("useEffect 동작 전에 실행되는 코드")
+        }
+    }, []) // mount할때만 작동
+
+
+    let [cnt, setCnt] = useState(0)
     return(
         <Container>
-            <Btn bg='blue'>버튼</Btn>
+            <Banner display= {display} className='alert alert-warning'>2초 이내 구매시 할인(css)</Banner>
+            {
+                alert?
+                <div className='alert alert-warning'>5초 이내 구매시 할인</div>
+                :null
+
+            }
+            
+            {cnt}
+            <Btn onClick={()=> setCnt(cnt+1)} bg='blue'>버튼</Btn>
             {idx<props.obj.length? 
             <Row>
                 <Col md={6}>
                     <img src={process.env.PUBLIC_URL + props.obj[idx].src} />
                 </Col>
                 <Col md={6}>
+                    {
+                    numRestrict?
+                    <div className='alert alert-danger'>숫자만 입력하세요</div>
+                    :null
+                    }
+                    <input type="text" onChange={(e)=>{setNumRestrict(isNaN(e.target.value))}}></input>
                     <h4 className="pt-5">{props.obj[idx].title}</h4>
                     <p>{props.obj[idx].content}</p>
                     <p>{props.obj[idx].price}</p>
